79a88926c62294801980feddce63bb19bb4b55c2
Bhaskar <105628455+Bhaskariitism@users.noreply.github.com>
2023-03-07 02:07:38 +0530

    Update reverse_num.c

--- reverse_num.c
+++ reverse_num.c
@@ -1,8 +1,9 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
     // Write C code here
-    printf("This Program is to check wheatgher a number is palindome or not\n");
+    printf("This Program is to reverse any given number\n");
     int n,rem = 0,rev;
     
     printf("Enter any positive number: - ");
@@ -10,12 +11,7 @@ int main() {
     while(n!=0){
     rem = n%10;
    rev = rev*10 + rem;
-  
-  
     n = n/10;
-    
-     
-    
     }
      printf("Reversed number = %d\n",rev);
     
